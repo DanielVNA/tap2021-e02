1606666703112238c531f0cc9b062c1d996f4438
DanielVNA <daniel.voinea99@gmail.com>
2021-04-16 08:39:03 +0300

    Started implementing the Sample class Corrected minor naming error

--- DanielVNA/src/tap2021-e02-S/tap2021-e02.Data/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace tap2021_e02.Data
-{
-    public class Class1
-    {
-    }
-}
---
+++ DanielVNA/src/tap2021-e02-S/tap2021-e02.Data/Sample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace tap2021_e02.Data
+{
+    public class Sample
+    {
+        public Sample(Guid Id, string DataSequence, DateTime AnalysisTime)
+        {
+
+        }
+
+
+    }
+}
