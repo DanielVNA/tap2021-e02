d7dfc96fb9b036c4e8a5f5949be4a2b01fc97312
DanielVNA <daniel.voinea99@gmail.com>
2021-04-16 09:33:03 +0300

    Implemented the extension of the required method

--- DanielVNA/src/tap2021-e02-S/tap2021-e02.Data/Sample.cs
+++ DanielVNA/src/tap2021-e02-S/tap2021-e02.Data/Sample.cs
@@ -4,10 +4,13 @@ namespace tap2021_e02.Data
 {
     public class Sample
     {
-        public Sample(Guid Id, string DataSequence, DateTime AnalysisTime)
+        public Sample(Guid Id, string dataSequence, DateTime AnalysisTime)
         {
 
         }
+
+        public string DataSequence { get; protected set; }
+
 
 
     }
--- DanielVNA/src/tap2021-e02-S/tap2021-e02.Extensions/IsValidDnaSequenceExtension.cs
+++ DanielVNA/src/tap2021-e02-S/tap2021-e02.Extensions/IsValidDnaSequenceExtension.cs
@@ -1,9 +1,26 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace tap2021_e02.Extensions
 {
-    public class IsValidDnaSequenceExtension
+    public static class IsValidDnaSequenceExtension
     {
+        public static bool StringCharactersValidation(string sampleTest)
+        {
+
+            Regex regExpr = new Regex("A|C|G|T");
+            bool containsAny = regExpr.IsMatch(sampleTest);
+            return containsAny;
+        }
+        public static bool IsValid(this Sample sample)
+        {
+            string SampleTest = sample.DataAnalysis;
+
+            if (SampleTest.Length == 1000 && StringCharactersValidation(SampleTest))
+                return true;
+            else
+                return false;
+        }
 
     }
 }
