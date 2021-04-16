76bd3a51fc103a4a11293bd08ef0f52ae4c26b4c
DanielVNA <daniel.voinea99@gmail.com>
2021-04-16 09:00:25 +0300

    Added basic structure to RepositoryPattern classes

--- DanielVNA/src/tap2021-e02-S/tap2021-e02.RepositoryPattern/RepoClass.cs
+++ DanielVNA/src/tap2021-e02-S/tap2021-e02.RepositoryPattern/RepoClass.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using tap2021_e02.Data;
 
 namespace tap2021_e02.RepositoryPattern
 {
-    class RepoClass
+    public RegisterSampleTransaction(
+        ISampleRepository<Sample> sampleRepository,
+        IUnitOfWork unitOfWork)
     {
+
     }
+
+    public void Execute(Sample sample)
+    {
+        ISampleRepository.InsertSample(sample);
+        IUnitOfWork.Commit();
+    }
+
+    
 }
--- DanielVNA/src/tap2021-e02-S/tap2021-e02.RepositoryPattern/RepoInterface.cs
+++ DanielVNA/src/tap2021-e02-S/tap2021-e02.RepositoryPattern/RepoInterface.cs
@@ -1,8 +1,21 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace tap2021_e02.RepositoryPattern
 {
-    public class RepoInterface
+    public interface ISampleRepository<TEntity>
     {
+        IEnumerable<TEntity> Query();
+        void InsertSample(TEntity entity);
+        void UpdateSample(TEntity entity);
+        void DeleteSample(TEntity entity);
+
+
+    }
+
+    public interface IUnitOfWork
+    {
+        void Commit();
     }
 }
